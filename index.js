c0349bc00a70868048067b4406a50b30760ebcf3
Nandu <nanduwastaken@gmail.com>
2023-05-04 11:46:18 +0530

    Now bots remembers what it said earlier

--- index.js
+++ index.js
@@ -35,12 +35,34 @@ client.on('messageCreate', async (message) => {
     prevMessages.forEach((msg) => {
       if (message.content.startsWith('!')) return;
       if (msg.author.id !== client.user.id && message.author.bot) return;
-      if (msg.author.id !== message.author.id) return;
+      // if (msg.author.id !== message.author.id) return; Why i am commenting this out becuase this thing ignore all the message other than the author who triggered this thing . it also ignores the bot itself . 
+    
+      if (msg.author.id == client.user.id) {  // checks if the msg id send by the bot itself 
 
-      conversationLog.push({
-        role: 'user',
-        content: msg.content,
-      });
+        conversationLog.push({
+          role: 'system',
+          content: msg.content,
+          name: msg.author.username, 
+        });
+
+        } else if (msg.author.id == message.author.id) {  // checks if the msg is send by the message author who triggered this
+
+      
+
+        conversationLog.push(
+          role: 'user',
+          content: msg.content,
+          name: message.author.username,
+        });
+
+
+
+       }
+     
+      // now the bot remembers the thing that the user said and also the things that the bot itself said
+      
+      return; // return nothing if the criteria of the chat for the message to be pushed is not correct
+      
     });
 
     const result = await openai
