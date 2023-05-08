ea0435180e484e85e2966e0052175d33b61a9532
notunderctrl <underctrl.io@gmail.com>
2023-05-08 14:45:00 +0300

    Fix null webhook url throwing errors

--- utils/logErrors.js
+++ utils/logErrors.js
@@ -2,14 +2,12 @@ const { WebhookClient } = require('discord.js');
 
 const WEBHOOK_URL = process.env.LOGS_WEBHOOK_URL;
 
-let webhookClient = new WebhookClient({ url: WEBHOOK_URL });
-
-console.log(webhookClient);
+let webhookClient = WEBHOOK_URL && new WebhookClient({ url: WEBHOOK_URL });
 
 module.exports = async (reason) => {
   try {
     if (!WEBHOOK_URL) {
-      console.log(`Error (No webhook URL found, using console):\n${reason}`);
+      console.log(`No webhook URL found, using console to log errors:\n${reason}`);
       return;
     }
 
