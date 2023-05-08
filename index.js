bb96fa9d9f01512ea3437f780d6cf99b69ad4537
Nandu <nanduwastaken@gmail.com>
2023-05-08 14:10:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,133 @@
+```js
 
+require('dotenv/config');
+
+const { Client, IntentsBitField } = require('discord.js');
+
+const { Configuration, OpenAIApi } = require('openai');
+
+const client = new Client({
+
+  intents: [
+
+    IntentsBitField.Flags.Guilds,
+
+    IntentsBitField.Flags.GuildMessages,
+
+    IntentsBitField.Flags.MessageContent,
+
+  ],
+
+});
+
+client.on('ready', () => {
+
+  console.log('The bot is online!');
+
+});
+
+const configuration = new Configuration({
+
+  apiKey: process.env.API_KEY,
+
+});
+
+const openai = new OpenAIApi(configuration);
+
+client.on('messageCreate', async (message) => {
+
+  if (message.author.bot) return;
+
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+
+  if (message.content.startsWith('!')) return;
+
+  let conversationLog = [
+
+    { role: 'system', content: 'You are a friendly chatbot.' },
+
+  ];
+
+  try {
+
+    await message.channel.sendTyping();
+
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg) => {
+
+      if (message.content.startsWith('!')) return;
+
+      if (msg.author.id !== client.user.id && message.author.bot) return;
+
+      if (msg.author.id == client.user.id) {
+
+        conversationLog.push({
+
+          role: 'assistant',
+
+          content: `${msg.author.username}: ${msg.content}`,
+
+          name: msg.author.username
+
+            .replace(/\s+/g, '_')
+
+            .replace(/[^\w\s]/gi, ''),
+
+        });
+
+      }
+
+      if (msg.author.id == message.author.id) {
+
+        conversationLog.push({
+
+          role: 'user',
+
+          content: `${msg.author.username}: ${msg.content}`,
+
+          name: message.author.username
+
+            .replace(/\s+/g, '_')
+
+            .replace(/[^\w\s]/gi, ''),
+
+        });
+
+      }
+
+    });
+
+    const result = await openai
+
+      .createChatCompletion({
+
+        model: 'gpt-3.5-turbo',
+
+        messages: conversationLog,
+
+        // max_tokens: 256, // limit token usage
+
+      })
+
+      .catch((error) => {
+
+        console.log(`OPENAI ERR: ${error}`);
+
+      });
+
+    message.reply(result.data.choices[0].message);
+
+  } catch (error) {
+
+    console.log(`ERR: ${error}`);
+
+  }
+
+});
+
+client.login(process.env.TOKEN);
+
+```
